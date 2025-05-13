34f3fe6ecbd5fb64af9905c027ae6fd2bfb6001f
Pavel <asinskijp188@gmail.com>
2025-05-13 21:13:13 +0300

    add class <Tasks> for distribute functions on clients requests

---
+++ task_manager.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from database_storage import DataBaseHub
+
+
+class Tasks:
+    """ Класс для определения нужной функции исходя из запроса клиента """
+
+    db_hub = DataBaseHub()
+    _commands: dict[str, callable] = {
+        'login': db_hub.registrate_new_player,
+        'logon': db_hub.logon_profile,
+        'check_unique': db_hub.check_login_on_unique
+    }
+
+    @staticmethod
+    def get_command(command: str) -> Union[callable, None]:
+        """ Определение функции по команде """
+        return Tasks._commands.get(command, None)
